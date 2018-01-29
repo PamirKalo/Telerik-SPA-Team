44bad124703203e7422723b9cbddf096ed9515e5
DESKTOP-FIP63CI\USER <dsd_k@abv.bg>
2018-01-29 15:05:44 +0200

    functionality equals card
    functionality equals card

--- scripts/main.js
+++ scripts/main.js
@@ -36,7 +36,8 @@ $(function () {
 $(function () {
     $("#createBoardBtn").on("click", createBoard);
     var hasBoard = false;
-
+    var arr = [1, 3, 5, 2, 6, 12, 8, 40, 3, 12, 8, 1, 5, 6, 40, 2];
+    var divValueMap = new Map();
     function createBoard(boardSize) {
         if (hasBoard) { // prevent creation infinity boards
             return;
@@ -48,15 +49,14 @@ $(function () {
             divBox,
             divRow;
 
-        var arr = [1, 3, 5, 2, 6, 12, 8, 40, 3, 12, 8, 1, 5, 6, 40, 2];
+        
         for (var i = 0; i < rowSize; i++) {
             divRow = $("<div class='row'>");
             for (var j = 0; j < colSize; j++) {
                 divBox = $("<div class='box'>")
                     .on('click', showCurrentCard);
                 divRow.append(divBox.attr('id', 'box' + idNumber));
-                divRow.append(divBox.attr('value', arr[idNumber-1]));
-                divRow.append(divBox.attr('open', false));
+                divValueMap.set('box'+idNumber, arr[idNumber-1]);
                 idNumber++;
             }
             divGameBoard.append(divRow);
@@ -67,21 +67,38 @@ $(function () {
     }
 
     var hasOpenCard = false;
-var arr = [1, 3, 5, 2, 6, 12, 8, 40, 3, 12, 8, 1, 5, 6, 40, 2];
+    var cardValueId = '';
     function showCurrentCard() {
         var el = $(this);
-        var idText=  el.attr("id");
-         //alert('id='+idText);
-         this.style.backgroundColor = "red";
-         var cardOpen = el.attr("open");
-        if (!cardOpen) {
-            $(this).toggleClass("rotated");
-            el.append(el.attr("value"));
-            el.attr("open", true);
-        }
-            
+
+         el.css("background-color", "red");
+        //  var currentCard=divValueMap.get(el.attr('id'));
          
+         el.html(divValueMap.get(el.attr('id')));
+         $(this).toggleClass("rotated");
+          //alert(divValueMap.get('currentCardId'));
+
+         if (hasOpenCard) {
+            if (divValueMap.get(cardValueId)===divValueMap.get(el.attr('id'))) {
+                alert('equals');        
+            } else {
+                alert('no');
+                el.css("background-color", "");
+                el.html("");
+                $("#"+cardValueId).html('');
+                $("#"+cardValueId).css("background-color", "");
+            }
+            hasOpenCard = false;
+            cardValueId='';
+         } else {
+             // to do 
+             //var currentCardId=el.attr('id');
+             cardValueId =el.attr('id');
+             hasOpenCard=true;
+         }
+        
     }
     
+
     
 });
