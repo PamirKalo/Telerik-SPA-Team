ceeadf7f77322aa95d591b7d0bc28b8815c84950
[VasilJivkov] <vasil_jivkov@hotmail.com>
2018-02-01 20:26:00 +0200

    merge
    merge of main func files

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,32 @@ $(function () {
     var boardSize = 8;
     var score = 0;
     var cardsType = 0;
+    var speedType = 0;
+	var speed;
+
+	if(speedType===0){
+	    speed = 1000;
+	} else if (speedType ===1) {
+	    speed = 2000;
+	} else if (speedType ===2) {
+	    speed = 3000;
+	}
+
+		var imageArr=["image1.jpg","image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg", "image7.jpg",
+	"image8.jpg", "image9.jpg", "image10.jpg", "image11.jpg", "image12.jpg", "image13.jpg", "image14.jpg", "image15.jpg",
+	"image16.jpg", "image17.jpg", "image18.jpg", "image19.jpg", "image20.jpg", ];
+
+	var colorArr = ['#5d8aa8','#f0f8ff','#e32636','#efdecd','#ffbf00','#a4c639','#cd9575','#915c83','#008000',
+	     '#fdee00','#66ff00','#004225','#480607','#cc5500','#702963','#c19a6b','#00cc99','#ffa700', '#ffffff', '#000000'];
+	     
+	var  wordArr = ['Variables', 'Math', 'Array', 'if-else', 'function', 'const', 'let', 'random', 'loop', 'for', 
+	'switch-case', 'object', 'regex', 'map', 'set', 'graph', 'three', 'link-list', 'scope', 'booleans',];
+
+	var arr = [];
 
     // slider
-    $("#submit").css("display", "none");
-
-    // hide the score_submit form by default
-    var handle = $("#custom-handle");
+    $("#submit").css("display", "none"); // hide the score_submit form by default
+    var handle = $("#custom-handle"); 
     $("#slider").slider({
         value: 8,
         min: 8,
@@ -54,70 +74,51 @@ $(function () {
     // var arr = [1, 3, 5, 2, 6, 12, 8, 40, 3, 12, 8, 1, 5, 6, 40, 2, 15, 7, 13, 7, 9, 15, 9, 13];
     var divValueMap = new Map();
 
-    // function createBoard(boardSize) {
+    
     function createBoard() {
         if (hasBoard) { // prevent creation infinity boards
             return;
         }
 
         cardsType = $("input[type='radio'][name='cardsType']:checked").val();
+
         $("#customize").hide(); // //hide the customization screen
         $("#submit").show(); //show the form for submitting score
 
-        var rowSize = 4, // Math.floor(Math.sqrt(boardSize*2))
-            colSize = boardSize * 2 / 4,
-            idNumber = 1,
-            divGameBoard = $("<div class='gameBoard' id='#gameBoard'>"),
-            divBox,
-            divRow,
-            startButton = $("<button>Start game!</button>");
+		var rowSize = 4; // Math.floor(Math.sqrt(boardSize*2))
+		var colSize = boardSize * 2 / 4;
+		var idNumber = 1;
+		var divGameBoard = $("<div class='gameBoard' id='#gameBoard'>");
+		var divBox;
+		var divRow;
+		var startButton = $("<button>Start game!</button>");
 
         function randDigits(max, min) {
             return Math.floor(Math.random() * (max - min)) + min;
         }
 
-        var arrfurst = [];
-        for (var w = 0; w < 50; w += 1) {
-            arrfurst.push({
-                value: w + 1,
-                visit: false,
-            });
-        }
+		var arrVisit = [];
+	    var input = [];
+	    for (var p=0; p<boardSize*2; p+=2) {
+	        var k=randDigits(0, 20);
+	        while (arrVisit[k]===1) {
+	            k=randDigits(0, 20);
+	        }
+	        input[p]=k;
+	        input[p+1]=k;
+	        arrVisit[k]=1;
+	    }
 
-        var input = [];
-        for (var p = 0; p < boardSize * 2; p += 2) {
+	    var arrVisitTwo=[];
+	    for (var t=0; t<boardSize*2; t+=1) {
+	        var m=randDigits(0, boardSize*2);
+	        while (arrVisitTwo[m]) {
+	            m=randDigits(0, boardSize*2);
+	        }                    
+	        arr[t]= input[m];
+	        arrVisitTwo[m]=true;
+	    }
 
-            var k = randDigits(0, 49);
-
-            while (arrfurst[k].visit) {
-                k = randDigits(0, 49);
-
-            }
-            input[p] = {
-                value: arrfurst[k].value,
-                visit: false,
-            };
-            input[p + 1] = {
-                value: arrfurst[k].value,
-                visit: false,
-            };
-            arrfurst[k].visit = true;
-        }
-
-
-        var arr = [];
-        for (var t = 0; t < boardSize * 2; t += 1) {
-
-            var m = randDigits(0, boardSize * 2);
-
-            while (input[m].visit) {
-                m = randDigits(0, boardSize * 2);
-
-            }
-            arr[t] = input[m].value;
-
-            input[m].visit = true;
-        }
 
         for (var i = 0; i < rowSize; i++) {
             divRow = $("<div class='row'>");
@@ -150,10 +151,19 @@ $(function () {
             return;
         }
         var el = $(this);
-
-        el.css("background-color", "red");
-        el.html(divValueMap.get(el.attr('id')));
+		var ind = divValueMap.get(el.attr("id"));
         $(this).toggleClass("rotated");
+
+        if (cardType===0){
+        el.addClass('cardFace');
+        el.css('background-image', `url(scripts/images/${imageArr[ind]})`);
+        } else if (cardType===1) {
+            el.html(`<div style='background-color:${colorArr[ind]};width: 100%;height:100%;'></div>`);
+        } else if (cardType===2) {
+            el.css("background-color", "red");
+            el.html(`${wordArr[ind]}`);
+        }
+
 
         if (hasOpenCard) {
             if (divValueMap.get(openedCardId) === divValueMap.get(el.attr('id'))) {
@@ -164,13 +174,20 @@ $(function () {
             } else {
                 var openedCard = $("#" + openedCardId);
                 inSetTimeOut = true;
-                setTimeout(function () {
-                    openedCard.html('');
+ 				setTimeout(function () {
+                    openedCard.html("");
                     openedCard.css("background-color", "");
+                    openedCard.css("background-image", "");
+                    openedCard.removeClass('cardFace');
+                    openedCard.toggleClass('rotated');
+                    el.removeClass('cardFace');
+                    el.css("background-image", "");
                     el.css("background-color", "");
                     el.html("");
+                    el.toggleClass('rotated');
+                    
                     inSetTimeOut = false;
-                }, 1000);
+                }, speed);
             }
             hasOpenCard = false;
             openedCardId = '';
@@ -188,3 +205,4 @@ $(function () {
         }
     }
 })
+
