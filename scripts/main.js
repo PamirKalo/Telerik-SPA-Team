4b56a1a69acd269a8d38b068bf053eb4a9ec4915
PamirKalo <kbm2@mail.bg>
2018-02-10 00:47:30 +0200

    update
    1. fix start game function
    2. animate progress bar
    3. added styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,39 @@
-
 $(function () {
     var boardSize = 8;
     var score = 0;
     var cardsType = 0;
     var speedType = 0;
-	var speed;
-    
-	if(speedType===0){
-	    speed = 1000;
-	} else if (speedType ===1) {
-	    speed = 2000;
-	} else if (speedType ===2) {
-	    speed = 3000;
-	}
+    var speed;
 
-	var imageArr=["image1.jpg","image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg", "image7.jpg",
-	"image8.jpg", "image9.jpg", "image10.jpg", "image11.jpg", "image12.jpg", "image13.jpg", "image14.jpg", "image15.jpg",
-	"image16.jpg", "image17.jpg", "image18.jpg", "image19.jpg", "image20.jpg", ];
+    if (speedType === 0) {
+        speed = 1000;
+    } else if (speedType === 1) {
+        speed = 2000;
+    } else if (speedType === 2) {
+        speed = 3000;
+    }
 
-	var colorArr = ['#5d8aa8','#f0f8ff','#e32636','#efdecd','#ffbf00','#a4c639','#cd9575','#915c83','#008000',
-	     '#fdee00','#66ff00','#004225','#480607','#cc5500','#702963','#c19a6b','#00cc99','#ffa700', '#ffffff', '#000000'];
-	     
-	var wordArr = ['Variables', 'Math', 'Array', 'if-else', 'function', 'const', 'let', 'random', 'loop', 'for',
-	'switch-case', 'object', 'regex', 'map', 'set', 'graph', 'three', 'link-list', 'scope', 'booleans',];
+    var imageArr = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg", "image7.jpg",
+        "image8.jpg", "image9.jpg", "image10.jpg", "image11.jpg", "image12.jpg", "image13.jpg", "image14.jpg", "image15.jpg",
+        "image16.jpg", "image17.jpg", "image18.jpg", "image19.jpg", "image20.jpg",
+    ];
 
-	var arr = [];
+    var colorArr = ['#5d8aa8', '#f0f8ff', '#e32636', '#efdecd', '#ffbf00', '#a4c639', '#cd9575', '#915c83', '#008000',
+        '#fdee00', '#66ff00', '#004225', '#480607', '#cc5500', '#702963', '#c19a6b', '#00cc99', '#ffa700', '#ffffff', '#000000'
+    ];
+
+    var wordArr = ['Variables', 'Math', 'Array', 'if-else', 'function', 'const', 'let', 'random', 'loop', 'for',
+        'switch-case', 'object', 'regex', 'map', 'set', 'graph', 'three', 'link-list', 'scope', 'booleans',
+    ];
+
+    var arr = [];
 
     // slider
     $("#submit").css("display", "none"); // hide the score_submit form by default
     $(".wiki").hide();
 
 
-    var handle = $("#custom-handle"); 
+    var handle = $("#custom-handle");
     $("#slider").slider({
         value: 8,
         min: 8,
@@ -73,27 +75,45 @@ $(function () {
     // create game 4x4 board when button is clicked
     $("#createBoardBtn").on("click", createBoard);
     var hasBoard = false;
+    // const startTimer = function () {
+    //     $('.progress-timer').timer({
+    //         format:'%H:%M:%S',
+    //         duration: '5m',
+    //         countdown: true,
+    //         callback: function() {
+    //             alert('Time up!');
+    //         }
+    //     });
+    //     var seconds = $('.progress-timer').data('seconds');
+    //     $('.progress-bar').addClass('progress-bar-striped active danger');
+    //     $('.progress').addClass('col-lg-12');
+    //     $('.progress-bar').css("width", parseInt(seconds) + "px");
+    // }
 
     const startTimer = function () {
         $('.progress-timer').timer({
-            format:'%H:%M:%S',
+            format: '%H:%M:%S',
             duration: '5m',
             countdown: true,
-            callback: function() {
+            callback: function () {
+                clearInterval(funcId);
                 alert('Time up!');
             }
         });
-        var seconds = $('.progress-timer').data('seconds');
-        $('.progress-bar').addClass('progress-bar-striped active danger');
-        $('.progress').addClass('col-lg-12');
-        $('.progress-bar').css("width", parseInt(seconds) + "px");
-    }
 
+        $('.progress-bar').addClass('progress-bar-striped active danger');
+
+        var funcId = setInterval(function () {
+            var partition = parseInt($('.progress-timer').data('seconds') * 100 / 300);
+            $('.progress-bar').css("width", partition + "%");
+            $('.progress-bar').css("aria-valuenow", partition + "%");
+        }, 1000)
+    }
 
     // var arr = [1, 3, 5, 2, 6, 12, 8, 40, 3, 12, 8, 1, 5, 6, 40, 2, 15, 7, 13, 7, 9, 15, 9, 13];
     var divValueMap = new Map();
 
-    
+
     function createBoard() {
         if (hasBoard) { // prevent creation infinity boards
             return;
@@ -104,39 +124,38 @@ $(function () {
         $("#submit").show(); //show the form for submitting score
         startTimer();
 
-		var rowSize = 4; // Math.floor(Math.sqrt(boardSize*2))
-		var colSize = boardSize * 2 / 4;
-		var idNumber = 1;
-		var divGameBoard = $("<div class='gameBoard' id='#gameBoard'>");
-		var divBox;
-		var divRow;
+        var rowSize = 4; // Math.floor(Math.sqrt(boardSize*2))
+        var colSize = boardSize * 2 / 4;
+        var idNumber = 1;
+        var divGameBoard = $("<div class='gameBoard' id='#gameBoard'>");
+        var divBox;
+        var divRow;
 
         function randDigits(max, min) {
             return Math.floor(Math.random() * (max - min)) + min;
         }
 
-		var arrVisit = [];
-	    var input = [];
-	    for (var p=0; p<boardSize*2; p+=2) {
-	        var k=randDigits(0, 20);
-	        while (arrVisit[k]===1) {
-	            k=randDigits(0, 20);
-	        }
-	        input[p]=k;
-	        input[p+1]=k;
-	        arrVisit[k]=1;
-	    }
-        
-	    var arrVisitTwo=[];
-	    for (var t=0; t<boardSize*2; t+=1) {
-	        var m=randDigits(0, boardSize*2);
-	        while (arrVisitTwo[m]) {
-	            m=randDigits(0, boardSize*2);
-	        }                    
-	        arr[t]= input[m];
-	        arrVisitTwo[m]=true;
-	    }
-        
+        var arrVisit = [];
+        var input = [];
+        for (var p = 0; p < boardSize * 2; p += 2) {
+            var k = randDigits(0, 20);
+            while (arrVisit[k] === 1) {
+                k = randDigits(0, 20);
+            }
+            input[p] = k;
+            input[p + 1] = k;
+            arrVisit[k] = 1;
+        }
+
+        var arrVisitTwo = [];
+        for (var t = 0; t < boardSize * 2; t += 1) {
+            var m = randDigits(0, boardSize * 2);
+            while (arrVisitTwo[m]) {
+                m = randDigits(0, boardSize * 2);
+            }
+            arr[t] = input[m];
+            arrVisitTwo[m] = true;
+        }
 
         for (var i = 0; i < rowSize; i++) {
             divRow = $("<div class='row'>");
@@ -165,19 +184,18 @@ $(function () {
             return;
         }
         var el = $(this);
-		var ind = divValueMap.get(el.attr("id"));
+        var ind = divValueMap.get(el.attr("id"));
         $(this).toggleClass("rotated");
-    
-        if (cardsType==='2'){
-        el.addClass('cardFace');
-        el.css('background-image', `url(scripts/images/${imageArr[ind]})`);
-        } else if (cardsType==='1') {
+
+        if (cardsType === '2') {
+            el.addClass('cardFace');
+            el.css('background-image', `url(scripts/images/${imageArr[ind]})`);
+        } else if (cardsType === '1') {
             el.html(`<div style='background-color:${colorArr[ind]};width: 100%;height:100%;'></div>`);
-        } else if (cardsType==='0') {
+        } else if (cardsType === '0') {
             el.css("background-color", "red");
             el.html(`${wordArr[ind]}`);
         }
-
 
         if (hasOpenCard) {
             if (divValueMap.get(openedCardId) === divValueMap.get(el.attr('id'))) {
@@ -188,7 +206,7 @@ $(function () {
             } else {
                 var openedCard = $("#" + openedCardId);
                 inSetTimeOut = true;
- 				setTimeout(function () {
+                setTimeout(function () {
                     openedCard.html("");
                     openedCard.css("background-color", "");
                     openedCard.css("background-image", "");
@@ -199,7 +217,7 @@ $(function () {
                     el.css("background-color", "");
                     el.html("");
                     el.toggleClass('rotated');
-                    
+
                     inSetTimeOut = false;
                 }, speed);
             }
@@ -211,30 +229,30 @@ $(function () {
         }
     }
 
-    $("#submitBtn").on("click", function () {    
+    $("#submitBtn").on("click", function () {
         var username = $('#username').val();
         if (username == "") {
             alert("Name must be filled out");
             return false;
         }
-         var keyusername = localStorage.getItem(username);
-         if (keyusername!==null &&  keyusername>score) {
-             score=keyusername;
-         } 
-         localStorage.setItem(username, score);
+        var keyusername = localStorage.getItem(username);
+        if (keyusername !== null && keyusername > score) {
+            score = keyusername;
+        }
+        localStorage.setItem(username, score);
 
     });
 
-    $('#leaderboard').on('click' ,function(e) {
+    $('#leaderboard').on('click', function (e) {
         console.log('heelo');
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
         $(".sidebar-nav").empty();
         var ranklistArr = Object.entries(localStorage);
-        ranklistArr.sort((a, b)=>(b[1]-a[1]));
+        ranklistArr.sort((a, b) => (b[1] - a[1]));
 
         //create holding list for the results
-        for (var r = 0; r<ranklistArr.length; r+=1) {
+        for (var r = 0; r < ranklistArr.length; r += 1) {
             $(".sidebar-nav").append(`<li class="list"> ${ranklistArr[r][0]} : ${ranklistArr[r][1]} </li>`);
         };
         //append the list to the div ranklist
